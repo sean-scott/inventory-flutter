8973740e5b470cb4b37b3ca83c6717baaa618365
Sean Scott <sean.scott894@gmail.com>
2017-06-04 18:16:28 -0700

    Updated item row UI

--- lib/item_row.dart
+++ lib/item_row.dart
@@ -21,7 +21,7 @@ class ItemRow extends StatelessWidget {
   final ItemRowActionCallback onIncreaseQuantity;
   final ItemRowActionCallback onDecreaseQuantity;
 
-  static const double kHeight = 150.0;
+  static const double kHeight = 120.0;
 
   GestureTapCallback _getHandler(ItemRowActionCallback callback) {
     return callback == null ? null : () => callback(item);
@@ -34,7 +34,6 @@ class ItemRow extends StatelessWidget {
       onDoubleTap: _getHandler(onDoubleTap),
       onLongPress: _getHandler(onLongPress),
       child: new Container(
-        padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 20.0),
         decoration: new BoxDecoration(
           border: new Border(
             bottom: new BorderSide(color: Theme.of(context).dividerColor)
@@ -43,32 +42,114 @@ class ItemRow extends StatelessWidget {
         child: new Row(
           children: <Widget>[
             new Expanded(
-              child: new Row(
-                children: <Widget>[
-                  new Expanded(
-                    flex: 2,
-                    child: new Text(
-                      item.name
-                    )
-                  ),
-                  new Column(
-                    children: <Widget>[
-                      new IconButton(
-                        icon: const Icon(Icons.keyboard_arrow_up),
-                        onPressed: _getHandler(onIncreaseQuantity),
-                      ),
-                      new Text(
-                        item.quantity.toString(),
-                      ),
-                      new IconButton(
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                        onPressed: _getHandler(onDecreaseQuantity),
-                      ),
-                    ]
-                  )
-                ]
+              flex: 3,
+              child: new Container(
+                child: new Icon(Icons.photo),
               )
-            )
+            ),
+            new Expanded(
+              flex: 6,
+              child: new Container(
+                child: new Column(
+                  children: [
+                    new Expanded(
+                      child: new Container(
+                        // empty for spacing
+                      )
+                    ),
+                    new Expanded(
+                      child: new Container(
+                        child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            new Text(item.name,
+                              style: new TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontStyle: FontStyle.italic,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    new Expanded(
+                      child: new Container(
+                        child: new Row(
+                          children: [
+                            new Expanded(
+                              child: new Icon(Icons.date_range, color: Colors.black54),
+                            ),
+                            new Expanded(
+                              flex: 2,
+                              child: new Text("12/31/2001", 
+                                style: new TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black54,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            new Expanded(
+                              child: new Container(
+                                // empty for spacing
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Expanded(
+              flex: 3,
+              child: new Container(
+                child: new Column(
+                  children: [
+                    new Expanded(
+                      child: new Container(
+                        child: new IconButton(
+                          icon: const Icon(
+                            Icons.keyboard_arrow_up,
+                            color: Colors.black54,
+                          ), 
+                          onPressed: _getHandler(onIncreaseQuantity)
+                        ),
+                      ),
+                    ),
+                    new Expanded(
+                      child: new Container(
+                        child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            new Text(item.quantity.toString(), 
+                              style: new TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black54,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    new Expanded(
+                      child: new Container(
+                        child: new IconButton(
+                          icon: const Icon(
+                            Icons.keyboard_arrow_down,
+                            color: Colors.black54,
+                          ), 
+                          onPressed: _getHandler(onDecreaseQuantity)
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ]
         )
       )
